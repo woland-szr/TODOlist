72a4951e01bd6f5fb6a68d5c70bfaec3ac804789
woland-szr <62295565+woland-szr@users.noreply.github.com>
2020-11-10 13:49:58 +0400

    Updates

---
+++ precache-manifest.16d61bfde616b0e7f7884d80f8d39a1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f08c8cc611058c833a277d8122a698",
+    "url": "/TODOlist/index.html"
+  },
+  {
+    "revision": "aaaef036ff6d4c9a0fad",
+    "url": "/TODOlist/static/css/main.a16874bd.chunk.css"
+  },
+  {
+    "revision": "7355bfb9abf32bd0e599",
+    "url": "/TODOlist/static/js/2.99ffd87e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TODOlist/static/js/2.99ffd87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaaef036ff6d4c9a0fad",
+    "url": "/TODOlist/static/js/main.4ad0df60.chunk.js"
+  },
+  {
+    "revision": "7c386d281c754c32303f",
+    "url": "/TODOlist/static/js/runtime-main.e7acb556.js"
+  }
+]);
